99d533c853e8302085820c549281f2b2a3a4398c
Nora <nora.hamacher@gmail.com>
2020-03-03 22:10:26 +1100

    Updates

---
+++ precache-manifest.2b477dc151b967506dad09455acfd69d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b785f3b356a625f9d3e180ceb110e5c",
+    "url": "/aussieElectro/index.html"
+  },
+  {
+    "revision": "6707f30799de0c1bba4e",
+    "url": "/aussieElectro/static/css/main.733361a6.chunk.css"
+  },
+  {
+    "revision": "1316956f31fc8cfa76e3",
+    "url": "/aussieElectro/static/js/2.ca8a4624.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/aussieElectro/static/js/2.ca8a4624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6707f30799de0c1bba4e",
+    "url": "/aussieElectro/static/js/main.965bc1d5.chunk.js"
+  },
+  {
+    "revision": "7e0fe0b1a8ec31d5cd6f",
+    "url": "/aussieElectro/static/js/runtime-main.55e4441f.js"
+  },
+  {
+    "revision": "b841217ab9e234ae1e9485ecc6280ac0",
+    "url": "/aussieElectro/static/media/facilities.b841217a.csv"
+  }
+]);
